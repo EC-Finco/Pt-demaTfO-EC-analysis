aa4b7a73386b82b85d858116cacf5f4f38780631
EC-Finco <matteo.finco.3@studenti.unipd.it>
2022-08-01 20:13:02 +0200

    Shifted to dataframes for ease of indexing and variables management

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import sys
 import importlib
 import paths
+import processing
 try:
     importlib.import_module('numpy')
     foundnp = True
@@ -56,7 +57,7 @@ warnings.filterwarnings("ignore", ".*No labelled objects found.*")
 
 
 # insert the path of the spectra
-path_in = input("Type the path of spectra: ")
+path_in = input("Type the path of CV: ")
 os.chdir(path_in)
 new_folders = input("create new folders for code output? [y/n]")
 path_smoothed, path_peaks = paths.folders_out(path_in, new_folders)
@@ -81,13 +82,58 @@ cv['dI'] = dI
 cv['dI_dE'] = dI_dE
 # plt.plot(cv.E, cv.I)
 # plt.plot(cv.E, cv.dI_dE)
-plt.plot(cv.dE)
-plt.show()
-#split scans
-inversion = [0 for i in range(len(cv.dE) - 1)]
+# plt.plot(cv.dE)
+# plt.show()
+# find inversions
+inversion = np.zeros(len(cv.dE))  # indexing variable
 for i in range(len(cv.dE)-1):
-    sgn = cv.dE[i] * cv.dE[i+1]
-    if sgn < 0:
+    if cv.dE[i]*cv.dE[i+1] <= 0:
         inversion[i] = i
-inversion = inversion[inversion != 0]
-print(dE)
+inversion = inversion[(inversion > 0)]  # removes empty elements
+# split scans
+n_scans = len(inversion)  # counts scans
+length_scans = np.zeros(n_scans)  # measures length of scan
+scan_dir = []  # label scan direction
+for i in range(n_scans-1):
+    length_scans[i] = int(inversion[i+1]-inversion[i])
+    if cv.dE[inversion[i]+1] >= 0:
+        scan_dir.append('anodic')
+    else:
+        scan_dir.append('cathodic')
+an_peaks = pd.DataFrame()
+cat_peaks = pd.DataFrame()
+for j in range(n_scans - 1):
+    index_start = int(inversion[j])
+    index_end = int(inversion[j+1])
+    scan = cv.iloc[index_start:index_end]
+    index = []
+    for i in range(int(length_scans[j])):
+        index.append(str(i+1))
+    scan.index = index
+    if scan_dir[j] == 'anodic':
+        peak_index = np.argmax(scan.I)
+        print(peak_index)
+        print(scan)
+        an_peaks.at[j,'Index'] = peak_index
+        an_peaks.at[j,'E_p'] = scan.E[peak_index]
+        an_peaks.at[j,'I_p'] = scan.I[peak_index]
+    elif scan_dir[j] = 'cathodic':
+        peak_index = np.argmax(scan.E)  # second derivative
+print(an_peaks)
+# # print(length_scans, scan_dir)  # use to probe the output
+# scan = np.array(1)
+# an_peaks = np.array(1)  # column 0 is index, column 1 is peak pot and column 2 is peak current
+# cat_peaks = np.array(1)
+# for j in range(n_scans - 1):
+#     scan[:, 0] = cv.E[inversion[j]:inversion[j+1]]
+#     scan[:, 1] = cv.I[inversion[j]:inversion[j+1]]
+#     if scan_dir[j] == 'anodic':
+#         an_peaks[j, 0] = np.argmax(scan[:, 1])
+#         an_peaks[j, 1:2] = scan[an_peaks[j, 0], :]
+#     # elif scan_dir[j] == 'cathodic':
+
+
+        # baselining to remove discharge or indexing maxima
+
+
+
---
+++ processing.py
@@ -0,0 +1,37 @@
+import numpy as np
+from scipy.signal import find_peaks, peak_prominences, peak_widths
+
+
+def peak_coll(cv, n_scans, scan_dir, inversion):
+    scans_I = []  # data from each scan in a different array
+    scans_E = []
+    peaks_indexs = []
+    peaks_I = []
+    peaks_E = []
+    peaks_pos = []
+    for j in range(n_scans - 1):
+        scans_I.append(np.array(0))
+        scans_I[j] = cv.I[int(inversion[j]):int(inversion[j + 1])]
+        scans_E.append(np.array(0))
+        scans_E[j] = cv.E[int(inversion[j]):int(inversion[j + 1])]
+        peaks_I.append(np.array(0))
+        peaks_E.append(np.array(0))
+        peaks_indexs.append(np.array(0))
+    #  peak finder
+    for i in range(n_scans - 1):
+        if scan_dir[i] == 'anodic':
+            peaks_pos = find_peaks(scans_I[i], distance=50)[0]
+            scan = scans_I[i]
+            peaks_I[i] = np.take(scan, peaks_pos)
+            scan = scans_E[i]
+            peaks_E[i] = np.take(scan, peaks_pos)
+        elif scan_dir[i] == 'cathodic':
+            peaks_pos = find_peaks(scans_I[i], distance=50)[0]
+            scan = scans_I[i]
+            peaks_I[i] = np.take(scan, peaks_pos)
+            scan = scans_E[i]
+            peaks_E[i] = np.take(scan, peaks_pos)
+        peaks_indexs[i] = peaks_pos
+        # to be continued...
+        # peak selector
+        # peaks_indexs[i] = peaks_indexs[i][scans_I[peaks_indexs[i]] == max(scans_I[peaks_indexs[i]])]
